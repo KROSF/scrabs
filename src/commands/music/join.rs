b7412059a65eb1d1d5c97d040c54fcafb371760b
Rodrigo Sanabria <rodrigosanabria22@gmail.com>
2020-05-31 19:42:02 +0200

    feat: added join command

---
+++ src/commands/music/join.rs
@@ -0,0 +1,58 @@
+use crate::common::{store::VoiceManager, utils::check_msg};
+
+use serenity::{
+    framework::standard::{macros::command, CommandResult},
+    model::{channel::Message, misc::Mentionable},
+    prelude::Context,
+};
+
+#[command]
+fn join(ctx: &mut Context, msg: &Message) -> CommandResult {
+    let guild = match msg.guild(&ctx.cache) {
+        Some(guild) => guild,
+        None => {
+            check_msg(
+                msg.channel_id
+                    .say(&ctx.http, "Groups and DMs not supported"),
+            );
+
+            return Ok(());
+        }
+    };
+
+    let guild_id = guild.read().id;
+
+    let channel_id = guild
+        .read()
+        .voice_states
+        .get(&msg.author.id)
+        .and_then(|voice_state| voice_state.channel_id);
+
+    let connect_to = match channel_id {
+        Some(channel) => channel,
+        None => {
+            check_msg(msg.reply(&ctx, "Not in a voice channel"));
+
+            return Ok(());
+        }
+    };
+
+    let manager_lock = ctx
+        .data
+        .read()
+        .get::<VoiceManager>()
+        .cloned()
+        .expect("Expected VoiceManager in ShareMap.");
+    let mut manager = manager_lock.lock();
+
+    if manager.join(guild_id, connect_to).is_some() {
+        check_msg(
+            msg.channel_id
+                .say(&ctx.http, &format!("Joined {}", connect_to.mention())),
+        );
+    } else {
+        check_msg(msg.channel_id.say(&ctx.http, "Error joining the channel"));
+    }
+
+    Ok(())
+}
